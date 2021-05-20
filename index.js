174acbc09f20632269f13be9d4a17e7797d5acd2
Wildan <wildanazzwa@gmail.com>
2021-05-20 22:03:32 +1000

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World' });
+});
+
+const PORT = 5000;
+
+app.listen(5000, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
